8a34c34b9a6831daeaaddf3612ba706e8a5808ec
mahi252008 <mahidaxeshpatel008@gmail.com>
2025-11-17 13:24:52 +0530

    stupid fix

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ load_dotenv()
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY")
     db_url = os.getenv("DATABASE_URL")
-    
+
     if db_url and db_url.startswith("postgres://"):
         db_url = db_url.replace("postgres://", "postgresql://", 1)
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = db_url
     SQLALCHEMY_TRACK_MODIFICATIONS = False
